5d8591b3b4038c4cd6430e1b5e6516cbb9d5a73f
Renan <renan081239@gmail.com>
2024-07-21 10:55:30 -0300

    first commit

---
+++ src/PlanoOperadora.java
@@ -0,0 +1,14 @@
+public class CaixaEletronico {
+    public static void main(String[] args){
+        String plano = "T";
+
+        switch (plano){
+            case "T":
+                System.out.println("5 gb de youtube");
+            case "M":
+                System.out.println("Whats e instagram grátis");
+            case "B":
+                System.out.println("50gb de internet");
+        }
+ }
+}
